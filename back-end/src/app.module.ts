47e7084ec59249583a828b90dbb366c8ee6efbc8
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-15 09:59:32 -0300

    :tada: Commit Inicial

---
+++ back-end/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ front-end/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-indigo-900 text-white text-2xl">EAE</h1>
+    </>
+  );
+}
+
+export default App;
